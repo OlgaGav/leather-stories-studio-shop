5dbe3f08337efe7f1d7f248d6a52eae5bb33a380
Olga Gavrushenko <ogavby+bb@gmail.com>
2026-03-20 13:53:30 -0700

    added one more log to debug shipping address null issue

--- server/config/shipping.js
+++ server/config/shipping.js
@@ -1,4 +1,4 @@
 // Allowed shipping countries for Stripe Checkout.
 // Uses ISO 3166-1 alpha-2 country codes.
 // Update this list to match current business/logistics coverage.
-export const ALLOWED_SHIPPING_COUNTRIES = ["US"];
+export const ALLOWED_SHIPPING_COUNTRIES = ['US'];
